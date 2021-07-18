678e80b165f072e4c675950b261289c8e1dc3634
Admin <Admin@host.docker.internal>
2021-07-18 16:09:09 +0530

    submit

---
+++ invoice-service/src/main/java/com/exaample/demo/App.java
@@ -0,0 +1,18 @@
+package com.exaample.demo;
+
+import com.exaample.demo.services.InvoiceService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        InvoiceService service = new InvoiceService();
+        
+    	System.out.println( "Hello World!" );
+    	
+    }
+}
---
+++ invoice-service/src/main/java/com/exaample/demo/services/InvoiceService.java
@@ -0,0 +1,10 @@
+package com.exaample.demo.services;
+
+public class InvoiceService {
+	
+	public String getInvoice() {
+		
+		return "Invoice: 101,'ramesh',4500";
+	}
+
+}
